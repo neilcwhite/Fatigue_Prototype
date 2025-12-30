9f8ccea9a5fe0fd394ee33800a67b6a4393a904e
neilcwhite <61410937+neilcwhite@users.noreply.github.com>
2025-12-30 18:41:41 +0000

    remove

--- src/components/auth/index.ts
+++
@@ -1,2 +0,0 @@
-export { AuthScreen } from './AuthScreen';
-export { SignOutHeader } from './SignOutHeader';
--- src/components/index.ts
+++
@@ -1,9 +0,0 @@
-// Components index - re-export from subfolders
-export * from './auth';
-export * from './ui';
-export * from './dashboard';
-export * from './planning';
-
-// Placeholder exports for components to be created:
-// export * from './calculator';
-// export * from './modals';
