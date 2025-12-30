3168d6862dce695f004fa1096d1615dfdae3f2d3
neilcwhite <61410937+neilcwhite@users.noreply.github.com>
2025-12-30 18:09:17 +0000

    rebuild

--- src/components/index.ts
+++
@@ -1,9 +0,0 @@
-// Components index - re-export from subfolders
-export * from './auth';
-export * from './ui';
-export * from './dashboard';
-
-// Placeholder exports for components to be created:
-// export * from './planning';
-// export * from './calculator';
-// export * from './modals';
